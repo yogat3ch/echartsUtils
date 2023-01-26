7acafffec322bfc53c381fadb834909a01598876
Stephen Holsenbeck <stephen@virgalabs.io>
2023-01-26 15:18:59 -0500

    Add `e_js_num2str`

--- R/js_helpers.R
+++ R/js_helpers.R
@@ -40,3 +40,35 @@ e_js_axisPointer <- function(sf = 0) {
             }
             ")
 }
+
+
+#' An JS callback formatting function
+#' @param js_parameters \code{chr} the parameters to pass to the Javascript Callback function
+#' @param n \code{chr} The javascript value to pass as `n`, the number to be transformed by `num2str`
+#' @param sf \code{int} significant figures of output
+#' @param add_suffix \code{lgl}  whether to add a letter suffix
+#' @param suffix_lb \code{chr} The lower bound of the divisor (IE "M" do not go lower than millions)
+#' @param format \code{lgl} whether to reduce the magnitude when formatting
+#' @param magnitude \code{chr} If the magnitude is known apriori this can be used to override the factoring
+#'
+#' @return The JS string with appropriate arguments
+#' @export
+
+e_js_num2str <- function(js_parameters = c("value", "index"), n = "value", sf = NULL, add_suffix = NULL, suffix_lb = NULL, format = NULL, magnitude = NULL) {
+  args <- purrr::compact(list(
+    sf = sf,
+    add_suffix = add_suffix,
+    suffix_lb = suffix_lb,
+    format = format,
+    magnitude = magnitude)) |>
+    jsonlite::toJSON(auto_unbox = TRUE) |>
+    stringr::str_remove_all("\"")
+
+  js_p <- glue::glue_collapse(js_parameters, sep = ", ")
+
+  return(UU::glue_js("(*{js_p}*) => {
+      vars args = *{args}*;
+      args.n = *{n}*;
+      return num2str(args);
+    }"))
+}
--- inst/src/js/num2str.js
+++ inst/src/js/num2str.js
@@ -1,35 +1,80 @@
-
 /**
 * @param {Number} n number to make into string
 * @param {Number} sf significant figures of output
+* @param {Boolean} add_suffix whether to add a letter suffix
+* @param {String} suffix_lb The lower bound of the divisor (IE "M" do not go lower than millions)
+* @param {Boolean} format whether to reduce the magnitude when formatting
+* @param {String} magnitude If the magnitude is known apriori this can be used to override the factoring
+* @returns {String} formatted axis label
 */
 
-function num2str (n, sf = 2) {
-
-function is_null (x) {
-  return x === null;
-}
-      if (!is_null(n)) {
-        var suf = ["", "K", "M", "B"];
-        var out = [];
-        for (var i = 1; i < 4; i++) {
-          out.push(n / (10 ** (i * 3)));
-        }
-        out = out.map(Math.abs);
-        var o = Math.min(...out.filter((a) => {return a>=1;}));
-        i = out.indexOf(o) + 1;
-        if (isFinite(o)) {
-          o = (o * Math.sign(n))
-          .toFixed(sf)
-          .replace(/[^\.]+0+$/gm, "")
-          .replace(/\.$/gm, "")
-          o = o + suf[i]
-        }  else {
-          o = n.toLocaleString()
+function num2str ({n, sf = 2, add_suffix = false, suffix_lb = "", format = true, magnitude = null} = {}) {
+  
+  var suf_is_string = typeof add_suffix == 'string';
+  var o = undefined;
+  if (!is_null(n)) {
+    if (format) {
+      if (suf_is_string && add_suffix == '%') {
+        if (is_likely_perc(n)) {
+          o = n * 100;
+        } else {
+          o = parseFloat(n);
         }
       } else {
-        var o = 'NA'
+        var suf = ["", "K", "M", "B"];
+        var out = [];
+        for (var i = 0; i < suf.length; i++) {
+          // test multiples of 1000
+          out.push(n / (10 ** (i * 3)));
+        }
+        if (magnitude !== null) {
+          var i = suf.indexOf(magnitude)
+          o = out[i]
+          // index of the lowest suffix requested
+          var lb = suf.indexOf(suffix_lb);
+        } else {
+          out = out.map(Math.abs);
+          // Which multiple of one thousand as the divisor results in a number above 1
+          var o = Math.min(...out.filter((a) => {return a>=1;}));
+          // The index of the suffix
+          i = out.indexOf(o);
+          // index of the lowest suffix requested
+          lb = suf.indexOf(suffix_lb);
+          // Replace infinity with 0
+          o = isFinite(o) ? o : 0.0;
+        }
       }
-
-  return o;
+     
+      
+      if (!suf_is_string && i > lb) {
+        // Convert to decimal with string suffix abbreviation format
+        o = (o * Math.sign(n))
+        .toFixed(sf);
+        // Exclude decimals from this logic that removes everything after the decimal
+        if (o >= 1 || o == 0 || o <= -1) {
+          o = o.replace(/[^\.]+0+$/gm, "")
+          .replace(/\.$/gm, "");
+        }
+        if (add_suffix) {
+          add_suffix = suf[i]; 
+        } 
+      } else {
+        // otherwise just format for humans
+        o = sigFig(n, sf = sf);
+      }
+    } 
+  } else {
+    var o = 'NA';
+  }
+  
+  
+  
+  if (typeof add_suffix == 'string') {
+     o = o.toLocaleString() + add_suffix;
+  } else {
+    if (typeof o == 'number') {
+      o = o.toLocaleString();
+    }
+  }
+return o;
 }
