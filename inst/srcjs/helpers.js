6435f2206855f03d0c115751d7a35d2c6b0307f2
Stephen Holsenbeck <stephen@virgalabs.io>
2023-06-27 17:01:14 -0400

    Add `Object.keep` method for filtering objects

--- inst/srcjs/helpers.js
+++ inst/srcjs/helpers.js
@@ -8,4 +8,12 @@ function is_null (x) {
   return x === null | x === undefined;
 }
 
-
+/** Adds a purrr::keep style function to Object
+/* @param obj {Object} object to be filtered
+/* @param predicate {Function} Function to be applied to each sub-object to determine if it should be kept
+/* @return {Object} with only the sub-objects matching the predicate function remaining
+**/
+Object.keep = (obj, predicate) =>
+    Object.keys(obj)
+          .filter( key => predicate(obj[key]) )
+          .reduce( (res, key) => (res[key] = obj[key], res), {} );
