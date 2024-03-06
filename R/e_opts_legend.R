2cbc884fec40d016fc0026e68b8f649175d957d9
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2024-03-06 18:59:59 -0500

    Add `as_chr` argument to `e_tooltip...`

--- R/e_opts_legend.R
+++ R/e_opts_legend.R
@@ -8,14 +8,14 @@
 #' @param width \code{chr} CSS compliant property
 #' @param margin_right \code{chr} CSS compliant property
 #' @param display \code{chr} CSS compliant property
+#' @param as_chr \code{lgl} whether the output should be HTML/character
 #' @param ... \code{chr} Additional CSS properties
-#'
 #' @return \code{shiny.tag}
 #' @export
 #'
 #' @examples
 #' e_tooltip_legend_dot("#48df94")
-e_tooltip_legend_dot <- function(background_color, color = "#000", border_radius = "10px", opacity = .2, height = "10px", width = "10px", margin_right = "4px", display = "inline-block", ...) {
+e_tooltip_legend_dot <- function(background_color, color = "#000", border_radius = "10px", opacity = .2, height = "10px", width = "10px", margin_right = "4px", display = "inline-block", as_chr = FALSE, ...) {
   props <- rlang::dots_list(...)
   props <- append(props,
                   list(
@@ -28,9 +28,12 @@ e_tooltip_legend_dot <- function(background_color, color = "#000", border_radius
                     width = width,
                     `margin-right` = margin_right
                   ))
-  htmltools::tags$span(
+  out <- htmltools::tags$span(
     style = css_props(declarations = props, inline = TRUE)
   )
+  if (as_chr)
+    out <- htmltools::doRenderTags(out)
+  return(out)
 }
 
 #' Create a rectangle legend item
@@ -42,7 +45,7 @@ e_tooltip_legend_dot <- function(background_color, color = "#000", border_radius
 #'
 #' @examples
 #' e_tooltip_legend_rectangle("#48df94")
-e_tooltip_legend_rectangle <- function(background_color, color = "#000", opacity = .2, height = "10px", width = "20px", display = "inline-block", ...) {
+e_tooltip_legend_rectangle <- function(background_color, color = "#000", opacity = .2, height = "10px", width = "20px", display = "inline-block", as_chr = FALSE, ...) {
 
   props <- rlang::dots_list(...)
   props <- append(props,
@@ -54,9 +57,12 @@ e_tooltip_legend_rectangle <- function(background_color, color = "#000", opacity
            height = height,
            width = width
          ))
-  htmltools::tags$span(
+  out <- htmltools::tags$span(
     style = css_props(declarations = props, inline = TRUE)
   )
+  if (as_chr)
+    out <- htmltools::doRenderTags(out)
+  return(out)
 }
 
 css_props <- function (...,
